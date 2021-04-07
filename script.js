62aa58cec3ed592e771939f992e9cb808854c352
anniec9205 <danbi9205@gmail.com>
2021-04-07 22:51:31 +1000

    added variables and functions and date and time

--- script.js
+++ script.js
@@ -2,3 +2,42 @@
 var searchButton = $(".searchButton");
 var apiKey = "ef5abdb71f846584dca1619109fb200e";
 
+//search button click event
+searchButton.click(function () {
+
+    var searchInput = $(".searchInput").val();
+
+    //variable for current weather  
+    var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+    //variable for 5 day forecast 
+    var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&Appid=" + apiKey + "&units=imperial";
+    
+    if (searchInput == "") {
+        console.log(searchInput);
+    } else {
+        $.ajax({
+            url: urlCurrent,
+            method: "GET"
+        }).then(function (response) {
+            //list-group append an li to it with just set text
+            //console.log(response.name);
+            var cityName = $(".list-group").addClass("list-group-item");
+            cityName.append("<li>" + response.name + "</li>");
+            //local storage
+            var local = localStorage.setItem(keyCount, response.name);
+            keyCount = keyCount + 1;
+
+            //adjust date 
+            var timeUTC = new Date(response.dt * 1000);
+            currentName.append(response.name + " " + timeUTC.toLocaleDateString("en-US"));
+            currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
+            //add temp 
+            var currentTemp = currentName.append("<p>");
+            //addClass("card-text");
+            currentName.append(currentTemp);
+            currentTemp.append("<p>" + "Temperature: " + response.main.temp + "</p>");
+            //add Humidity
+            currentTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>")
+        });
+    }
+});
