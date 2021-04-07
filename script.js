0edaab13aacbe08590ffd87e3fe5245516e1521b
anniec9205 <danbi9205@gmail.com>
2021-04-07 23:09:10 +1000

    the 5 days forecast added

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ searchButton.click(function () {
             //console.log(response.name);
             var cityName = $(".list-group").addClass("list-group-item");
             cityName.append("<li>" + response.name + "</li>");
+            
             //local storage
             var local = localStorage.setItem(keyCount, response.name);
             keyCount = keyCount + 1;
@@ -37,7 +38,26 @@ searchButton.click(function () {
             currentName.append(currentTemp);
             currentTemp.append("<p>" + "Temperature: " + response.main.temp + "</p>");
             //add Humidity
-            currentTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>")
+            currentTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
+        });
+        
+        //calling for 5-day forecast 
+        $.ajax({
+            url: urlFiveDay,
+            method: "GET"
+        }).then(function (response) {
+            //array for 5 days 
+            var day = [0, 8, 16, 24, 32];
+            var fiveDayCard = $(".fiveDayCard").addClass("card-body");
+            var fiveDayDiv = $(".fiveDayOne").addClass("card-text");
+            fiveDayDiv.empty();
+            //for each 5 days
+            day.forEach(function (i) {
+                var FiveDayTimeUTC1 = new Date(response.list[i].dt * 1000);
+                FiveDayTimeUTC1 = FiveDayTimeUTC1.toLocaleDateString("en-US");
+                fiveDayDiv.append("<div class=fiveDayColor>" + "<p>" + FiveDayTimeUTC1 + "</p>" + `<img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png">` + "<p>" + "Temperature: " + response.list[i].main.temp + "</p>" + "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>" + "</div>");
+            })
         });
     }
 });
+
